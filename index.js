f7c143690f6233e36462737cb45f8ad2ea2703df
Atul <adivedi@poshn.co>
2024-01-15 14:41:07 +0530

    make db connection

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./utils/db');
 const port = 3000;
-
+connectDB()
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
